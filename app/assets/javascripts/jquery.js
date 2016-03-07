b8f08bb08d12a0fdbcc6e9a233d9398118e376ad
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-03-07 20:45:40 +0000

    Add bold font Rob and PT with their js css and script and change in nav links to pt bold

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -20,3 +20,16 @@ $('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
     var s = document.getElementsByTagName('script')[0];
     s.parentNode.insertBefore(wf, s);
   })(); </script>
+  
+  <script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'PT+Sans+Narrow:700:latin', 'PT+Sans:700:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
