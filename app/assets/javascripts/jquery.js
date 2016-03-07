18869b30902a119353a2405f8c6ad6b42c65ca75
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-03-07 20:34:33 +0000

    Add Google api for Roboto and PT fonts js, script and css

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -6,3 +6,17 @@ $('#bs-example-navbar-collapse-1').on('show.bs.collapse', function() {
 $('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
     $('.nav-pills').removeClass('nav-stacked');
 });
+
+
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Roboto+Slab::latin', 'PT+Sans+Narrow::latin', 'PT+Sans::latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
