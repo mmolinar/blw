3d38007f4b1566dc7f48809d7db16552740a09b1
mariamolinaryarza@gmail.com <mariamolinaryarza@gmail.com>
2016-03-23 20:59:01 +0000

    js

--- app/assets/javascripts/jquery.js
+++ app/assets/javascripts/jquery.js
@@ -8,15 +8,15 @@ $('#bs-example-navbar-collapse-1').on('hide.bs.collapse', function() {
 });
 
 
-  <script type="text/javascript">
-  WebFontConfig = {
-    google: { families: [ 'PT+Sans+Narrow:400,700:latin', 'PT+Sans:400,700:latin', 'Roboto+Slab::latin' ] }
-  };
-  (function() {
-    var wf = document.createElement('script');
-    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-  })(); </script>
+  // <script type="text/javascript">
+  // WebFontConfig = {
+  //   google: { families: [ 'PT+Sans+Narrow:400,700:latin', 'PT+Sans:400,700:latin', 'Roboto+Slab::latin' ] }
+  // };
+  // (function() {
+  //   var wf = document.createElement('script');
+  //   wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  //   wf.type = 'text/javascript';
+  //   wf.async = 'true';
+  //   var s = document.getElementsByTagName('script')[0];
+  //   s.parentNode.insertBefore(wf, s);
+  // })(); </script>
